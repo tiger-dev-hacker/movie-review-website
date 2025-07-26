c0c973484a6ee4828e493a975543b631c224188d
Suril Waghmare <waghmaresuril@gmail.com>
2025-07-26 11:21:51 -0400

    Synchronized frontend and backend

--- frontend/movie-client/src/App.js
+++ frontend/movie-client/src/App.js
@@ -1,36 +1,69 @@
+
 import './App.css';
-import api from './api/axiosConfig'; 
-import { useState, useEffect } from 'react';
+import api from './api/axiosConfig';
+import {useState, useEffect} from 'react';
 import Layout from './components/Layout';
-import { Routes, Route } from 'react-router-dom'; 
+import {Routes, Route} from 'react-router-dom';
 import Home from './components/home/Home';
 import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
+import Reviews   from './components/reviews/Reviews';
 function App() {
-  const [movies, setMovies] = useState(); 
 
-  const getMovies = async () => {
+  const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState([]);
 
-    try {
-          const response = await api.get("/api/v1/movies"); 
-          console.log(response.data); 
-          setMovies(response.data); 
-    } catch(err)
+  const getMovies = async () =>{
+    
+    try
     {
-          console.log(err); 
+
+      const response = await api.get("/api/v1/movies");
+
+      setMovies(response.data);
+
+    } 
+    catch(err)
+    {
+      console.log(err);
     }
+  }
+
+  const getMovieData = async (movieId) => {
+     
+    try 
+    {
+        const response = await api.get(`/api/v1/movies/${movieId}`);
+
+        const singleMovie = response.data;
+
+        setMovie(singleMovie);
+
+        setReviews(singleMovie.reviews);
+        
+
+    } 
+    catch (error) 
+    {
+      console.error(error);
+    }
+
   }
 
   useEffect(() => {
     getMovies();
-  }, []); 
+  },[])
 
   return (
     <div className="App">
-      <Header />
+      <Header/>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Home movies={movies} />}></Route>
-        </Route>
+          <Route path="/" element={<Layout/>}>
+            <Route path="/" element={<Home movies={movies} />} ></Route>
+            <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+            <Route path="/api/v1/reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
+          </Route>
       </Routes>
 
     </div>
